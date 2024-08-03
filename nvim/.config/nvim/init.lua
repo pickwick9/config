f9c90e4003ea13db1e4ea9bdb398ee6ed33d9c8c
allen <allen@pop-os.mynetworksettings.com>
2024-08-03 02:19:20 -0400

    smartcase

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,4 +3,6 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.clipboard = "unnamedplus"
