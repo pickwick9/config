01260221fc2689b66b15395da44ccc0b78dfd9f4
allen <allen@pop-os.mynetworksettings.com>
2024-10-30 07:29:38 -0500

    changes

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y build-essential curl gpg gzip tar wget 
+sudo apt install -y build-essential curl gpg gzip tar wget
 sudo apt install -y tmux neovim
 sudo apt install -y git-all python3-pip
 sudo apt install -y gparted htop moreutils neofetch psensor strace
 sudo apt install -y libpthread-stubs0-dev thermald xclip
-sudo apt install -y clamav lynis openvpn wireshark 
+sudo apt install -y clamav lynis openvpn wireshark
 sudo apt install -y adb android-sdk-platform-tools-common
 sudo apt install -y ffmpeg gimp pavucontrol qbittorrent vlc xournalpp
 pip install yt-dlp spotdl
